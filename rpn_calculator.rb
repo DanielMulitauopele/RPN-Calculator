f6a485dacd1b4c2b8f8d388164675c556a6e572c
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2019-03-09 19:11:23 -0700

    Changes gets.chomp in if loop to variable, was being called twice.

--- rpn_calculator.rb
+++ rpn_calculator.rb
@@ -1,15 +1,18 @@
 puts 'Welcome to the RPN Calculator. Please input a calculation.'
 
 running_calculation = true
-numbers = []
 current_value = 0
+numbers = []
 
 while running_calculation
-  numbers << gets.chomp
+  user_input = gets.chomp
 
-  if gets.chomp == "q"
+  if user_input == "q"
     running_calculation = false
+    puts "Calculation terminated. Goodbye!"
   end
+
+  numbers << user_input.to_i unless user_input == "q"
 end
 
 print numbers
