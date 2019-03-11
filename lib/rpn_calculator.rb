45504bd8f97f72c5dba2e69c291c34f72b152296
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2019-03-11 16:26:49 -0600

    Revert "Adds gemfile, test helper, and initial test for the rpn calculator class"
    This reverts commit 93fce9043bdf82d118e4541e9b9a9519e448d6f0.

--- lib/rpn_calculator.rb
+++ lib/rpn_calculator.rb
@@ -1,38 +1,35 @@
-class RPNCalculator
-  # running_calculation = true
-  # numbers = []
-  #
-  # while running_calculation
-  #   user_input = gets.chomp
-  #
-  #   if user_input == "q"
-  #     running_calculation = false
-  #     puts "Calculation terminated. Goodbye!"
-  #   elsif user_input == "+"
-  #     sum = numbers[-2] + numbers[-1]
-  #     numbers = numbers[0..-3]
-  #     numbers << sum
-  #     puts "= #{sum}"
-  #   elsif user_input == "-"
-  #     diff = numbers[-2] - numbers[-1]
-  #     numbers = numbers[0..-3]
-  #     numbers << diff
-  #     puts "= #{diff}"
-  #   elsif user_input == "*"
-  #     product = numbers[-2] * numbers[-1]
-  #     numbers = numbers[0..-3]
-  #     numbers << product
-  #     puts "= #{product}"
-  #   elsif user_input == "/"
-  #     quot = numbers[-2] / numbers[-1]
-  #     numbers = numbers[0..-3]
-  #     numbers << quot
-  #     puts "= #{quot}"
-  #   else
-  #     numbers << user_input.to_i
-  #   end
-  # end
-  #
-  # print numbers
+running_calculation = true
+numbers = []
 
+while running_calculation
+  user_input = gets.chomp
+
+  if user_input == "q"
+    running_calculation = false
+    puts "Calculation terminated. Goodbye!"
+  elsif user_input == "+"
+    sum = numbers[-2] + numbers[-1]
+    numbers = numbers[0..-3]
+    numbers << sum
+    puts "= #{sum}"
+  elsif user_input == "-"
+    diff = numbers[-2] - numbers[-1]
+    numbers = numbers[0..-3]
+    numbers << diff
+    puts "= #{diff}"
+  elsif user_input == "*"
+    product = numbers[-2] * numbers[-1]
+    numbers = numbers[0..-3]
+    numbers << product
+    puts "= #{product}"
+  elsif user_input == "/"
+    quot = numbers[-2] / numbers[-1]
+    numbers = numbers[0..-3]
+    numbers << quot
+    puts "= #{quot}"
+  else
+    numbers << user_input.to_i
+  end
 end
+
+print numbers
--- test/test_helper.rb
+++
@@ -1,6 +0,0 @@
-require 'simplecov'
-SimpleCov.start
-
-require 'minitest/autorun'
-require 'minitest/pride'
-require 'pry'
