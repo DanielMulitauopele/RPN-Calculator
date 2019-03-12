a4bfb1ad7df47619b2fc80fb5165a9d99eb7e35c
Daniel Mulitauopele <daniel.mulitauopele@gmail.com>
2019-03-12 12:23:08 -0600

    Adds tests for operator methods

--- test/rpn_calculator_test.rb
+++ test/rpn_calculator_test.rb
@@ -16,4 +16,47 @@ class RPNCalculatorTest < MiniTest::Test
 
     assert_equal expected, actual
   end
+
+  def test_verify_operands_fail
+    expected = "There are not enough operands to complete this operation. Please ensure there are at least two operands.\n"
+    actual = @calculator.verify_operands('+')
+
+    assert_output( stdout = expected ) { @calculator.verify_operands('+') }
+  end
+
+  def test_sum_method
+    @calculator.numbers = [5, 6]
+    @calculator.sum
+    expected = [11]
+    actual = @calculator.numbers
+
+    assert_equal expected, actual
+  end
+
+  def test_subtract_method
+    @calculator.numbers = [6, 3]
+    @calculator.subtract
+    expected = [3]
+    actual = @calculator.numbers
+
+    assert_equal expected, actual
+  end
+
+  def test_multiply_method
+    @calculator.numbers = [3, 3]
+    @calculator.multiply
+    expected = [9]
+    actual = @calculator.numbers
+
+    assert_equal expected, actual
+  end
+
+  def test_divide_method
+    @calculator.numbers = [9, 3]
+    @calculator.divide
+    expected = [3]
+    actual = @calculator.numbers
+
+    assert_equal expected, actual
+  end
 end
